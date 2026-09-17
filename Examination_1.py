e10bbea82bfad0e82e85ac801b1e77925d55cb4b
Silnwza <siraporn.prom1999@gmail.com>
2026-09-17 09:58:36 +0700

    Improve input prompts and output for payment calculation

--- Examination_1.py
+++ Examination_1.py
@@ -6,5 +6,6 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-pay = int(input())
-price = int(input())
+pay=int(input("จำนวนเงินที่จ่าย:"))
+price=int(input("ราคาสินค้า:"))
+print("จำนวนเงินทอน:",pay-price)
